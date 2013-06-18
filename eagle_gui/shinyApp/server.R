af044f0d42142a73f9b4a86e440175f55866aa7c
aaronjfisher <aaron.fisher@rochester.edu>
2013-06-18 17:26:46 -0400

    Update server.R
    again, to make the merging hopefully easier

--- eagle_gui/shinyApp/server.R
+++ eagle_gui/shinyApp/server.R
@@ -1,1 +1,281 @@
-#nothing
+
+cat("source'ing code...", file=stderr())
+source("Adaptive_Group_Sequential_Design.R", local=TRUE)
+
+st<-read.csv(file= "sliderTable.csv",header=TRUE,as.is=TRUE)
+bt<-read.csv(file= "boxTable.csv",header=TRUE,as.is=TRUE)
+allVarNames<-c(st[,'inputId'],bt[,'inputId'])
+
+shinyServer(function(input, output) {
+
+  Apply_button <- -1
+
+  # In interactive mode, we re-export the parameters and rebuild table1
+  # every time.  In batch, only on the first call for a given push of the
+  # Apply button.
+  regen <- function(apply_button_value) {
+#cat("regen:", input$Batch, "\n", file=stderr())
+  if (input$Batch == "1" && apply_button_value <= Apply_button)
+	    return()
+
+	if (input$Batch == "1")
+	    isolate(for(i in 1:length(allVarNames))
+		assign(allVarNames[i], input[[allVarNames[i]]], inherits=TRUE)
+	    )
+	else
+	    for(i in 1:length(allVarNames))
+		assign(allVarNames[i], input[[allVarNames[i]]], inherits=TRUE)
+
+#cat("making table1 ...")
+	table1 <<- table_constructor()
+#cat("Done\n")
+
+	if (input$Batch == "1")
+		Apply_button <<- apply_button_value
+  }
+
+# apply_button <- quote({
+# 	val <- input$Parameters
+# 	if (val == 0)
+# 		return("")
+# 	regen(val)
+# 	#as.character(val)
+# 	""
+# })
+# output$params <- renderText(apply_button, quoted=TRUE)
+
+  output$power_curve_plot <- renderPlot({
+	regen(input$Parameters)
+	power_curve_plot()
+  })
+
+  output$expected_sample_size_plot <- renderPlot({
+	regen(input$Parameters)
+	expected_sample_size_plot()
+  })
+
+  output$expected_duration_plot <- renderPlot({
+	regen(input$Parameters)
+	expected_duration_plot()
+  })
+
+  overruns <- function() plot(1:2, main="Overruns")
+  output$overruns <- renderPlot({
+	regen(input$Parameters)
+	overruns()
+  })
+
+# HJ - x must be a list of length 3, with a digits and caption
+xtable <- function(x) {
+	xtable::xtable(x[[1]], digits=x$digits, caption=x$caption)
+}
+
+# HJ - see shiny:::htmlEscape (why is this necessary?)
+renderTable <- function (expr, ..., env = parent.frame(), quoted = FALSE, func = NULL) 
+{
+    if (!is.null(func)) {
+        shinyDeprecated(msg = "renderTable: argument 'func' is deprecated. Please use 'expr' instead.")
+    }
+    else {
+        func <- exprToFunction(expr, env, quoted)
+    }
+    function() {
+        classNames <- getOption("shiny.table.class", "data table table-bordered table-condensed")
+        data <- func()
+        if (is.null(data) || identical(data, data.frame())) 
+            return("")
+        return(paste(capture.output(print(xtable(data, ...), 
+            type = "html", html.table.attributes = paste("class=\"", 
+                #htmlEscape(classNames, TRUE), "\"", sep = ""), 
+                shiny:::htmlEscape(classNames, TRUE), "\"", sep = ""), 
+            ...)), collapse = "\n"))
+    }
+}
+
+  output$adaptive_design_sample_sizes_and_boundaries_table.2 <-
+  output$adaptive_design_sample_sizes_and_boundaries_table <- renderTable({
+	regen(input$Parameters)
+	adaptive_design_sample_sizes_and_boundaries_table()
+  })
+
+  output$fixed_H0C_design_sample_sizes_and_boundaries_table.2 <-
+  output$fixed_H0C_design_sample_sizes_and_boundaries_table <- renderTable({
+	regen(input$Parameters)
+	fixed_H0C_design_sample_sizes_and_boundaries_table()
+  })
+
+  output$fixed_H0S_design_sample_sizes_and_boundaries_table.2 <-
+  output$fixed_H0S_design_sample_sizes_and_boundaries_table <-renderTable({
+	regen(input$Parameters)
+	fixed_H0S_design_sample_sizes_and_boundaries_table()
+  })
+
+  output$performance_table <- renderTable({
+	regen(input$Parameters)
+	transpose_performance_table(performance_table())
+    })
+
+})
+=======
+cat("source'ing code...", file=stderr())
+
+#Get the csv file either online or locally
+getItOnline<-TRUE
+try({
+  source("Adaptive_Group_Sequential_Design.R", local=TRUE)
+  st<-read.csv(file= "sliderTable.csv",header=TRUE,as.is=TRUE)
+  bt<-read.csv(file= "boxTable.csv",header=TRUE)
+  getItOnline<-FALSE #if we haven't gotten an error yet!
+},silent=TRUE)
+try({
+  if(getItOnline){
+    library(devtools)
+    library(RCurl)
+    library(digest)
+    st<-read.csv(text= getURL("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp/sliderTable.csv"),header=TRUE,as.is=TRUE)
+    bt<-read.csv(text=getURL("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp/boxTable.csv"),header=TRUE,as.is=TRUE)
+    source_url("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp/Adaptive_Group_Sequential_Design.R")
+  }
+},silent=TRUE)
+
+cat("...got code!...", file=stderr())
+
+allVarNames<-c(st[,'inputId'],bt[,'inputId'])
+
+# T.adaptive <- NULL
+# T.fixed_H0C <- NULL
+# T.fixed_H0S<- NULL
+# T.performance_table <- NULL
+
+# cat("making table1...", file=stderr())
+# table1 <- table_constructor()
+# cat("Done\n", file=stderr())
+# print(str(table1))
+
+
+
+
+
+
+shinyServer(function(input, output) {
+
+regen <-reactive({
+
+  for(i in 1:length(allVarNames)){
+    stringToDo<-paste0(allVarNames[i],' <<- input$', allVarNames[i])
+    eval(parse(text=stringToDo))
+  }
+
+
+  # p1_user_defined <<- input$p1_user_defined
+  # p10_user_defined  <<- input$p10_user_defined
+  # p20_user_defined  <<- input$p20_user_defined
+  # p11_user_defined  <<- input$p11_user_defined
+  # p21_user_defined  <<- input$p21_user_defined
+  # alpha_FWER_user_defined <<- input$alpha_FWER_user_defined
+  # alpha_H0C_user_defined  <<- input$alpha_H0C_user_defined
+  # per_stage_sample_size_combined_adaptive_design_user_defined <<- input$per_stage_sample_size_combined_adaptive_design_user_defined
+  # per_stage_sample_size_when_only_subpop_2_enrolled_adaptive_design_user_defined  <<- input$per_stage_sample_size_when_only_subpop_2_enrolled_adaptive_design_user_defined
+  # Delta <<- input$Delta
+  # iter  <<- input$iter
+  # total_number_stages <<- input$total_number_stages
+  # recruitment_rate_subpop_1 <<- input$recruitment_rate_subpop_1
+  # recruitment_rate_subpop_2 <<- input$recruitment_rate_subpop_2
+  # lower_bound_treatment_effect_subpop_2 <<- input$lower_bound_treatment_effect_subpop_2
+  # upper_bound_treatment_effect_subpop_2 <<- input$upper_bound_treatment_effect_subpop_2
+  # last_stage_subpop_2_enrolled_adaptive_design  <<- input$last_stage_subpop_2_enrolled_adaptive_design
+  # H0C_futility_boundary_proportionality_constant_adaptive_design  <<- input$H0C_futility_boundary_proportionality_constant_adaptive_design
+  # H0S_futility_boundary_proportionality_constant_adaptive_design  <<- input$H0S_futility_boundary_proportionality_constant_adaptive_design
+  # H0C_futility_boundary_proportionality_constant_fixed_design <<- input$H0C_futility_boundary_proportionality_constant_fixed_design
+  # H0S_futility_boundary_proportionality_constant_fixed_design <<- input$H0S_futility_boundary_proportionality_constant_fixed_design
+
+cat("making table...")
+  table1<<-table_constructor()
+
+  T.adaptive <<- 
+	adaptive_design_sample_sizes_and_boundaries_table()
+
+  T.fixed_H0C <<-
+	fixed_H0C_design_sample_sizes_and_boundaries_table()
+
+  T.fixed_H0S<<-
+	fixed_H0S_design_sample_sizes_and_boundaries_table()
+
+  T.performance_table <<- transpose_performance_table(performance_table())
+
+cat("Done\n")
+})
+
+  output$power_curve_plot <- renderPlot({
+cat(input$p1_user_defined, "...", file=stderr())
+	regen()
+	power_curve_plot()
+cat("Done\n", file=stderr())
+  })
+
+  output$expected_sample_size_plot <- renderPlot({
+cat(input$p1_user_defined, "...", file=stderr())
+	regen()
+	expected_sample_size_plot()
+cat("Done\n", file=stderr())
+  })
+
+  output$expected_duration_plot <- renderPlot({
+cat(input$p1_user_defined, "...", file=stderr())
+	regen()
+	expected_duration_plot()
+cat("Done\n", file=stderr())
+  })
+
+  overruns <- function() plot(1:2, main="Overruns")
+  output$overruns <- renderPlot({
+cat(input$p1_user_defined, "...", file=stderr())
+	regen()
+	overruns()
+cat("Done\n", file=stderr())
+  })
+
+# HJ - removed the ... from the print call
+renderTable <- function (expr, ..., env = parent.frame(), quoted = FALSE, func = NULL) 
+{
+    if (!is.null(func)) {
+        shinyDeprecated(msg = "renderTable: argument 'func' is deprecated. Please use 'expr' instead.")
+    }
+    else {
+        func <- exprToFunction(expr, env, quoted)
+    }
+    function() {
+        classNames <- getOption("shiny.table.class", "data table table-bordered table-condensed")
+        data <- func()
+        if (is.null(data) || identical(data, data.frame())) 
+            return("")
+        return(paste(capture.output(print(xtable(data, ...),
+		type = "html",
+		html.table.attributes = paste(
+			"class=\"", shiny:::htmlEscape(classNames, TRUE),
+		"\"", sep = ""))), collapse = "\n"))
+    }
+}
+
+  output$adaptive_design_sample_sizes_and_boundaries_table <- renderTable({
+	regen()
+	T.adaptive[[1]]
+  }, digits=T.adaptive$digits, caption=T.adaptive$caption)
+		
+  output$fixed_H0C_design_sample_sizes_and_boundaries_table <-
+	renderTable({
+    regen()
+		T.fixed_H0C[[1]]
+  }, digits=T.fixed_H0C$digits, caption= T.fixed_H0C$caption)
+
+  output$fixed_H0S_design_sample_sizes_and_boundaries_table <-renderTable({
+    regen()
+    T.fixed_H0S[[1]]
+  },digits=T.fixed_H0S$digits, caption=T.fixed_H0S$caption)
+
+  output$performance_table <- renderTable({
+    regen()
+    T.performance_table[[1]]
+    },digits=T.performance_table$digits, caption=T.performance_table$caption)
+
+})
