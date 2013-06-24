adf0a6e174daa973cc013b1ad7d3a1f405a11c17
Aaron Fisher <afisher@jhsph.edu>
2013-06-24 15:21:33 -0400

    adding glimmer scripts

---
+++ eagle_gui/glimmer files/ui.R
@@ -0,0 +1,16 @@
+#install.packages(RCurl)
+#install.packages(devtools)
+#install.packages(digest)
+
+#install.packages(c("httr", "RCurl", "memoise", "whisker", "evaluate"))
+#download.file('http://cran.rstudio.com/src/contrib/Archive/devtools/devtools_1.1.tar.gz',
+#              destfile='devtools_1.1.tar.gz')
+#install.packages('devtools_1.1.tar.gz', type='source', repos=NULL)
+#unlink('devtools_1.1.tar.gz')
+
+library(shiny)
+library(digest)
+library(RCurl)
+library(devtools)
+
+source_url('https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp/ui.R')
