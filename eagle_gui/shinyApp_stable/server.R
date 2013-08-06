583eadbee0ff578f2dda49f2bfd482ac4623cc5e
Aaron Fisher <afisher@jhsph.edu>
2013-08-05 23:48:49 -0400

    adding a stable folder for spark, removing H01 from captions so we just have H0S

---
+++ eagle_gui/shinyApp_stable/server.R
@@ -0,0 +1,449 @@
+#BUGS!
+# Add Michael's Warnings
+# Add a checkbox to autoupdate new parameters, or a warning saying they're not yet applied.
+# Need to make sure the initialized image is set correctly. 
+    # For beta testing it's just commented out to maintain consistency.
+
+#############################################
+#############################################
+# PREAMBLE
+#############################################
+#############################################
+
+
+cat("source'ing code...", file=stderr())
+
+#Get the csv file either online or locally
+getItOnline<-TRUE
+try({
+  source("Adaptive_Group_Sequential_Design.R", local=TRUE)
+  st<-read.csv(file= "sliderTable.csv",header=TRUE,as.is=TRUE)
+  bt<-read.csv(file= "boxTable.csv",header=TRUE,as.is=TRUE)
+  getItOnline<-FALSE #if we haven't gotten an error yet!
+  cat("found code locally...", file=stderr())
+},silent=TRUE)
+try({
+  if(getItOnline){
+    library(devtools)
+    library(RCurl)
+    library(digest) #some reason this is a dependency not auto-loaded by devtools?
+    st<-read.csv(text= getURL("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp_stable/sliderTable.csv"),header=TRUE,as.is=TRUE)
+    bt<-read.csv(text=getURL("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp_stable/boxTable.csv"),header=TRUE,as.is=TRUE)
+    source_url("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp_stable/Adaptive_Group_Sequential_Design.R")
+    cat("found code online...", file=stderr())
+  }
+},silent=TRUE)
+
+cat("...got code!...", file=stderr())
+
+allVarNames<-c(st[,'inputId'],bt[,'inputId'])
+lastAllVars<-rep(0,length(allVarNames))
+names(lastAllVars)<-allVarNames
+
+for(i in 1:dim(st)[1]){
+  assign(st[i,'inputId'], st[i,'value'])
+  lastAllVars[st[i,'inputId']] <- st[i,'value']
+}
+for(i in 1:dim(bt)[1]){
+  assign(bt[i,'inputId'], bt[i,'value']) 
+  lastAllVars[bt[i,'inputId']] <- bt[i,'value']
+}
+
+#table1 <- table_constructor() #getting error in x y length differing when we call plots the first time, if we call table_constructor too soon?
+
+
+
+
+
+#THIS LAST BIT HAS BEEN TRICKY TO GET WORKING! Maybe better to start with just a 
+# whole image file, but for beta testing just leave it as is.
+
+# buildTable1Now<-TRUE
+# if(!getItOnline){
+#   try({
+#     load(file='initialized_image_&_table1.RData')
+#     cat("loaded table1 & image...", file=stderr())
+#     buildTable1Now<-FALSE
+#   })
+# }
+# if(buildTable1Now){
+#   table1 <- table_constructor()
+#   cat("built table1...", file=stderr())
+# }
+# Saving table1 for future use
+# iter<-10000
+# save.image(file='initialized_image_&_table1.RData')
+
+
+
+
+
+#############################################
+#############################################
+# END OF PREAMBLE
+#############################################
+#############################################
+
+
+
+
+shinyServer(function(input, output) {
+
+
+  #############################################
+  #############################################
+  # Initialize some static & reactive variables
+  #############################################
+  #############################################
+
+
+  lastApplyValue <- -1
+  totalCalls<-0 #a place keeper to watch when we cat to stderr
+  uploadFileTicker<-0
+  inValues<-NULL
+
+  #current value of all the data, need to store this if we want to change the sliders to all be animated in interactive mode, but not change their values.
+  #all used a comparison against static lastAllVars
+  #Also do error checks for invalid inputs
+  allVars<-reactive({
+    x<-c()
+    for(i in 1:length(allVarNames)) x[allVarNames[i]]<- input[[allVarNames[i] ]]
+
+    #Check to make sure all box inputs are within the required ranges. 
+    #Don't need to do this for sliders, since you can't set them past the min/max
+    minMaxErrs<-rep('',dim(bt)[1]) #vector to store error/warnings messages. The ith entry is '' if allVars()[i] is valid
+    for(i in 1:dim(bt)[1]){
+      nameInd<- i+dim(st)[1]
+      minMaxErrs_ind<-FALSE
+      if( x[allVarNames[nameInd]]>bt[i,'max'])  {
+        x[allVarNames[nameInd]]<-bt[i,'max']
+        minMaxErrs_ind<-TRUE
+      }
+      if( x[allVarNames[nameInd]]<bt[i,'min'])  {
+        x[allVarNames[nameInd]]<-bt[i,'min']
+        minMaxErrs_ind<-TRUE
+      }
+      if(minMaxErrs_ind) minMaxErrs[i]<- paste0('Warning: the variable "',bt[i,'label'], '" exceeds the allowed range, and has been set to ',x[allVarNames[nameInd]],'. ')
+    }
+    output$warn3<-renderText({paste(minMaxErrs,collapse='')})
+
+    #Other error checks on inputs.
+    warn2<-""
+    if(x['total_number_stages']<x['last_stage_subpop_2_enrolled_adaptive_design']){
+        x['last_stage_subpop_2_enrolled_adaptive_design']<-x['total_number_stages']
+        warn2<-paste("Warning: the last stage sub population 2 is enrolled must be less than the total number of stages. Here the last stage in which sub population 2 is enrolled is set to",x['total_number_stages'],"the total number of stages")
+    }
+    output$warn2<-renderText({warn2})
+
+    #Done! Send back the error-checked list of inputs
+    x
+  })
+
+
+  params<-reactive({ input$Parameters1 + input$Parameters2 })
+
+  #when we're not just looking at basic parameters, we force batch mode.
+  effectivelyBatch<- reactive({input$Batch == "1" | input$Which_params != "1"})
+
+
+  #############################################
+  #############################################
+  # Warnings
+  #############################################
+  #############################################
+  output$warn1<-renderText({ #STILL NEED TO MAKE WORK!
+    x<-""
+    if(input$Which_params!="1" & input$Batch=="2")x<-"Note: interactive mode not enabled for advanced parameters, defaulting to batch mode."
+    x    
+  })
+#See also the allVars() for other errors
+  
+
+
+  #############################################
+  #############################################
+  # LOADING DATA
+  #############################################
+  #############################################
+
+  #a reactive chunk to feed to the dynamicBoxes & dynamicSliders
+  regenUpload<-reactive({
+    upFile <- input$uploadData
+    x<-NULL
+    if (!is.null(upFile)){
+      x<-c(read.csv(file=upFile$datapath, row.names=1, header=FALSE))[[1]]
+      names(x)<-allVarNames
+      #regenCalls<-0
+      uploadFileTicker<<-0
+      output$uploadTime<<-renderText({as.character(Sys.time())})
+      print('resetting upload')
+    }
+    inValues<<-x
+  })
+
+
+
+
+
+
+
+  #############################################
+  #############################################
+  # SLIDERS & BOXES
+  #############################################
+  #############################################
+
+
+  #NOTE - June 26: When sliders update, regen thinks it needs to be called again because sliders have updated values and you're now in interactive mode.
+        #solution -- added lastAllVars (nonreactive) variable that cancels this out.
+  #explanation of uploadFileTicker: Only if it's the first time do we change the sliders to the uploaded values. If it's not the first time, we use the current value of the variable, taken from allVars.
+  sliders <- reactive({ #NOTE: if you upload the same file again it won't update b/c nothing's techincally new!!!
+    print('sliders')
+    print(uploadFileTicker)
+    regenUpload() #reactive input to uploaded file
+    sliderList<-list()
+    animate<-FALSE
+    if(input$Batch=='2' & input$Which_params=='1' ) animate<-TRUE # reactive input
+    for(i in 1:dim(st)[1]){
+      #case1: null upfile & zero uploadFileTicker (sliders haven't changed yet)
+      value_i<-st[i,'value']
+      #case2: something uploaded, and hasn't been called yet (uploadFileTicker==0)
+      if(!is.null(inValues)) value_i<-inValues[st[i,'inputId']]
+      #case3: whatever's there has been called already, ignore uploaded data.
+      if(uploadFileTicker>0) isolate(value_i<-allVars()[st[i,'inputId']])
+      sliderList[[i]]<-sliderInput(inputId=st[i,'inputId'], label=st[i,'label'], min=st[i,'min'], max=st[i,'max'], value=value_i, step=st[i,'step'], animate=animate)
+    }
+    uploadFileTicker<<-uploadFileTicker+1
+    print('           sliders updating!!!!!!')
+    sliderList
+  })
+  output$fullSliders<-renderUI({sliders()})
+
+
+  boxes <- reactive({ #NOTE: if you upload the same file again it won't update b/c nothing's techincally new!!!
+    print('                    boxes')
+    regenUpload()
+    boxList<-list()
+    for(i in 1:dim(bt)[1]){
+      value_i<-bt[i,'value']
+      if( (!is.null(inValues)) ){ value_i<-inValues[bt[i,'inputId']]}
+      boxList[[i]]<-numericInput(inputId=bt[i,'inputId'], label=bt[i,'label'], min=bt[i,'min'], max=bt[i,'max'], value=value_i, step=bt[i,'step'])
+    }
+    boxList
+  })
+  output$fullBoxes<-renderUI({boxes()})
+
+
+
+
+
+
+
+
+
+  #############################################
+  #############################################
+  # REGEN
+  #############################################
+  #############################################
+
+
+  # In interactive mode, we re-export the parameters and rebuild table1
+  # every time.  In batch, only on the first call for a given push of the
+  # Apply button.
+  #argument applyValue is usally fed in as params(), the sum of the two apply buttons
+
+
+
+  regen <- reactive({
+    applyValue<-params()
+    totalCalls<<-totalCalls+1
+    print(paste('total calls =',totalCalls))
+    #ESCAPE --
+    #escape if it's called when dynamic sliders/buttons are still loading
+    if(is.null(input$Batch) || is.null(applyValue))
+      {print('regen null batch or apply -> out') ; return()}
+    for(name in allVarNames){
+      isolate(if(is.null(input[[name]])) {print('null regen input -> out');return()})
+    }
+    #in batch mode: if buttons have NOT been pressed since last time, 
+  	if (  effectivelyBatch() && (applyValue <= lastApplyValue) )
+  	    return()
+    #In batch or interactive, check for no change -- especially useful for slider asthetic changes.
+        #no need to isolate this next line -- if we're not in interative mode we would have already exited by now.
+    if(all(allVars()==lastAllVars)) {
+      print('no change')
+      lastApplyValue <<- applyValue #fixes bug where if you hit apply w/ no changes, the next slider change will interactively call table_constructor()
+      return()
+    }
+
+
+    #if Batch==1, we don't want things updating automatically, so we use isolate()
+    #JUNE 26 - This must be done in conjunction with isolating the is.null tests @ the start of the function
+    #need to use allVars() so that we head off some potential errors
+    assignAllVars<-function(){
+      for(i in 1:length(allVarNames))
+        assign(allVarNames[i], allVars()[allVarNames[i]], inherits=TRUE)
+    }
+    cat("assigning Variables ...")
+  	if (effectivelyBatch()){ isolate(assignAllVars() )
+    } else { assignAllVars() }
+
+    #browser()
+    cat("making table1 ...")
+    table1 <<- table_constructor()
+    cat("Done\n")
+
+  	if (effectivelyBatch() ) lastApplyValue <<- applyValue
+    lastAllVars<<-allVars()
+})
+
+
+
+
+
+  #############################################
+  #############################################
+  # PLOTS
+  #############################################
+  #############################################
+
+
+  
+  table1 <<- table_constructor() #temporary fix -- moving this initializer down to just before it's called, to make sure all the settings are adjusted right.
+
+
+
+  #############
+  #Performance plots
+
+  output$power_curve_plot <- renderPlot({
+	regen() 
+  print('power plot')
+	power_curve_plot()
+  })
+
+  output$expected_sample_size_plot <- renderPlot({
+	regen()
+  print('sample plot')
+	expected_sample_size_plot()
+  })
+
+  output$expected_duration_plot <- renderPlot({
+	regen()
+  print('duration plot')
+	expected_duration_plot()
+  })
+
+  #overruns <- function() plot(1:2, main="Overruns")
+  output$overruns <- renderPlot({
+	regen()
+  print('overruns plot')
+	overrun_plot()
+  })
+
+
+  #############
+  #Boundary Plots
+  output$fixed_HOC_boundary_plot<-renderPlot({
+    regen()
+    print('H0C Boundary Plot')
+    boundary_fixed_HOC_plot()
+  })
+
+  output$fixed_HOS_boundary_plot<-renderPlot({
+    regen()
+    print('H0S Boundary Plot')
+    boundary_fixed_HOS_plot()
+  })
+
+  output$adapt_boundary_plot<-renderPlot({
+    regen()
+    print('H0C Boundary Plot')
+    boundary_adapt_plot()
+  })
+
+
+
+
+
+  #############################################
+  #############################################
+  # TABLES (& new xtable/renderTable functions)
+  #############################################
+  #############################################
+
+# HJ - x must be a list of length 3, with a digits and caption
+xtable <- function(x) {
+	xtable::xtable(x[[1]], digits=x$digits, caption=x$caption) #NEED TO EXPAND HERE!!
+}
+
+# HJ - see shiny:::htmlEscape (why is this necessary?)
+renderTable <- function (expr, ..., env = parent.frame(), quoted = FALSE, func = NULL, include.colnames=TRUE) 
+{
+    if (!is.null(func)) {
+        shinyDeprecated(msg = "renderTable: argument 'func' is deprecated. Please use 'expr' instead.")
+    }
+    else {
+        func <- exprToFunction(expr, env, quoted)
+    }
+    function() {
+        classNames <- getOption("shiny.table.class", "data table table-bordered table-condensed")
+        data <- func()
+        if (is.null(data) || identical(data, data.frame())) 
+            return("")
+        return(paste(capture.output(print(xtable(data, ...), include.colnames=include.colnames, 
+            type = "html", html.table.attributes = paste("class=\"", 
+                #htmlEscape(classNames, TRUE), "\"", sep = ""), 
+                shiny:::htmlEscape(classNames, TRUE), "\"", sep = ""), 
+            ...)), collapse = "\n"))
+    }
+}
+
+  output$adaptive_design_sample_sizes_and_boundaries_table.2 <-
+  output$adaptive_design_sample_sizes_and_boundaries_table <- renderTable({    
+	regen()
+  print('adapt table')
+	adaptive_design_sample_sizes_and_boundaries_table()
+  })
+
+  output$fixed_H0C_design_sample_sizes_and_boundaries_table.2 <-
+  output$fixed_H0C_design_sample_sizes_and_boundaries_table <- renderTable({
+	regen()
+  print('HOC table')
+	fixed_H0C_design_sample_sizes_and_boundaries_table()
+  })
+
+  output$fixed_H0S_design_sample_sizes_and_boundaries_table.2 <-
+  output$fixed_H0S_design_sample_sizes_and_boundaries_table <-renderTable({
+	regen()
+  print('HOS table')
+	fixed_H0S_design_sample_sizes_and_boundaries_table()
+  })
+
+  output$performance_table <- renderTable(expr={
+	regen()
+  print('perf table')
+	transpose_performance_table(performance_table())
+    },include.colnames=FALSE)
+
+
+
+
+
+
+  #############################################
+  #############################################
+  #SAVE DATA
+  #############################################
+  #############################################
+  output$downloadData <- downloadHandler(
+    filename =  'inputs.csv',
+    content = function(file) {
+      inputCsv<-rep('',length=length(allVarNames))
+      for(i in 1:length(allVarNames)) inputCsv[i]<- input[[ allVarNames[i] ]]
+      write.table(inputCsv, file, row.names=allVarNames, col.names=FALSE, sep=',')
+    }
+  )
+
+})
---
+++ eagle_gui/shinyApp_stable/ui.R
@@ -0,0 +1,258 @@
+# ## Subpopulation 1 proportion (Range: 0 to 1)
+# p1 <- 0.61
+
+# ## Prob. outcome = 1 under control:
+# ## for Subpop. 1 (Range: 0 to 1)
+# p10_user_defined <- 0.33
+# ## for Subpop. 2 (Range: 0 to 1)
+# p20_user_defined <- 0.12
+
+# ## Prob. outcome = 1 under treatment, at alternative:
+# ## for Subpop. 1 (Range: 0 to 1)
+# p11_user_defined<- 0.33 + 0.125
+# ## for Subpop. 2 (Range: 0 to 1)
+# p21_user_defined<- 0.12 + 0.125
+
+
+# non-standard plot dimensions
+width <- "90%"          # narrower
+height <- "500px"       # more than 400
+
+my_plotOutput <- function(...)
+	plotOutput(..., width=width, height=height)
+
+# NOTE: header element defaults to h3 instead of h1
+my_headerPanel <- function (title, windowTitle = title, h=h3)
+{
+    tagList(tags$head(tags$title(windowTitle)), div(class = "span12", 
+        style = "padding: 10px 0px;", h(title)))
+}
+
+# from Aaron
+#Load csv's with info about the input sliders & boxes
+#then build lists of input sliders & boxes
+#slider table & box table
+
+
+#H0Ctext<-as.character(div(HTML("H<sub>0C</sub>")))
+#H0Stext<-as.character(div(HTML("H<sub>0S</sub>")))
+
+H0Ctext<-HTML("H<sub>0C</sub>")
+H0Stext<-HTML("H<sub>0S</sub>")
+
+
+#Get the csv file either online or locally
+getItOnline<-TRUE
+try({
+  source("Adaptive_Group_Sequential_Design.R", local=TRUE)
+  st<-read.csv(file= "sliderTable.csv",header=TRUE,as.is=TRUE)
+  bt<-read.csv(file= "boxTable.csv",header=TRUE,as.is=TRUE)
+  cat("found code locally...", file=stderr())
+  
+  #### action buttons
+  # No reason for all the action buttons to do the same thing, so
+  # we add the resource here once. 
+  # Maybe we should test that ./actionbutton/actionbutton.js exists. #AF - this is now incoporated I think :)
+  # We have stolen this file from shinyIncubator.
+  # suppressMessages(addResourcePath(
+  #     prefix='actionbutton', 
+  #     directoryPath=file.path(getwd(), 'actionbutton')
+  # ))  
+      
+  # # adapted from shinyIncubator, so we don't require that package
+  # my_actionButton <- function(inputId, label) {
+  #   tagList(
+  #     singleton(tags$head(tags$script(src = 'actionbutton/actionbutton.js'))),
+  #     tags$button(id=inputId, type="button", class="btn action-button", label)
+  #   )
+  # }
+  ################
+  getItOnline<-FALSE #if we haven't gotten an error yet!
+
+},silent=TRUE)
+try({
+  if(getItOnline){
+
+    library(devtools)
+    library(RCurl)
+    library(digest) #some reason this is a dependency not auto-loaded by devtools?
+    st<-read.csv(text= getURL("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp_stable/sliderTable.csv"),header=TRUE,as.is=TRUE)
+    bt<-read.csv(text=getURL("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp_stable/boxTable.csv"),header=TRUE,as.is=TRUE)
+    source_url("https://raw.github.com/aaronjfisher/Adaptive_Shiny/master/eagle_gui/shinyApp_stable/Adaptive_Group_Sequential_Design.R")
+    
+    #################
+    # TO USE SHINY 0.5 instead of 0.6
+    # Stolen from shinyIncubator
+    # needs actionbutton/actionbutton.js
+    suppressMessages(addResourcePath(
+        prefix='actionbutton',
+        directoryPath=file.path(getwd(), 'actionbutton')
+    ))
+    # adapted from shinyIncubator, so we don't require that package
+    my_actionButton <- function(inputId, label) {
+      tagList(
+        singleton(tags$head(tags$script(src = 'actionbutton/actionbutton.js'))),
+        tags$button(id=inputId, type="button", class="btn action-button", label)
+      )
+    }
+    actionButton <- my_actionButton
+    #################
+
+    cat("found code online...", file=stderr())
+  }
+},silent=TRUE)
+
+
+
+
+
+
+
+#################################
+
+
+allVarNames<-c(st[,'inputId'],bt[,'inputId'])
+
+sliderList<-list()
+boxList<-list()
+
+#build the inputs that will be seen
+#THIS IS NO LONGER USED!!!!?!?!: Since sliders & boxes are now dynamic objects defined in the server.
+#building sliders
+for(i in 1:dim(st)[1]){
+sliderList[[i]]<-sliderInput(inputId=st[i,'inputId'], label=st[i,'label'], min=st[i,'min'], max=st[i,'max'], value=st[i,'value'], step=st[i,'step'], animate=st[i,'animate'])
+}
+names(sliderList)<-st[,'inputId']
+
+#building numerical input boxes
+for(i in 1:dim(bt)[1]){
+boxList[[i]]<-numericInput(inputId=bt[i,'inputId'], label=bt[i,'label'], min=bt[i,'min'], max=bt[i,'max'], value=bt[i,'value'], step=bt[i,'step'])
+}
+names(boxList)<-bt[,'inputId']
+################################
+
+
+  animationOptions(interval = 5000, loop = FALSE,
+  playButton = NULL, pauseButton = NULL)
+
+shinyUI(pageWithSidebar(
+
+
+  my_headerPanel("Multi-stage design tool"),  
+
+
+
+
+
+  # "Advanced" forces batch mode
+  sidebarPanel(
+        #TOP PANEL
+
+        selectInput("Which_params", "", c("Show basic parameters" = "1",
+                "Show advanced parameters" = "2", "Show all parameters" = "3") ),
+
+        #SAVE & LOAD
+        conditionalPanel(condition="input.Which_params == '3'",
+            br(),
+            strong('Save current parameters to file:'),br(),
+            downloadButton('downloadData', 'Save'),
+            br(),br(),
+            strong('Load previous parameters from file:'),
+            fileInput('uploadData', '',
+                    accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+            conditionalPanel(condition='false', #need to make this actually contional!?
+              strong('Reset parameters to uploaded file:'),br(),
+              actionButton(inputId='loadReset',label='Reset')
+            ),
+            strong("Current Parameters:")
+          ),
+
+        #INTERACTIVE MODE ONLY IF JUST SLIDERS
+          #Note interactive mode is auto-disabled if you're not in the "just sliders" view
+        conditionalPanel(condition= "input.Which_params== '1'",
+          selectInput("Batch", "", c("Batch mode" = "1",
+                "Interactive mode" = "2"))
+        ),
+        #BASIC SLIDERS
+        conditionalPanel(condition = "input.Which_params == '1' || input.Which_params == '3'",
+          #show apply button if you're in batch mode, or showing all inputs and batch mode is enforced
+          conditionalPanel(condition='input.Batch== "1" || input.Which_params== "3" ',
+            actionButton("Parameters1", "Apply")),
+          #uiOutput('actionButton'),
+          br(), br(),
+          uiOutput('fullSliders')
+        ),
+        #ADVANCED BOXES
+        conditionalPanel(condition = "input.Which_params == '2' || input.Which_params == '3'",
+          #always show apply button
+          actionButton("Parameters2", "Apply"),
+          #uiOutput('actionButton'),
+          br(), br(),
+          uiOutput('fullBoxes'))
+  ),
+
+
+  # # "Advanced" forces batch mode
+  # sidebarPanel(
+  #       selectInput("Which_params", "", c("Basic parameters" = "1",
+  #               "Advanced parameters" = "2")),
+  #       conditionalPanel(condition = "input.Which_params == '1'",
+  #               selectInput("Batch", "", c("Batch mode" = "1",
+  #                       "Interactive mode" = "2")),
+  #               conditionalPanel(condition = "input.Batch == '1'",
+  #                       my_actionButton("Parameters", "Apply"),
+  #                       br(), br()),
+  #               sliderList),
+  #       conditionalPanel(condition = "input.Which_params == '2'",
+  #               my_actionButton("Parameters", "Apply"),
+  #               br(), br(),
+  #               boxList)
+  # ),
+
+
+  mainPanel(
+  #WARNINGS
+  h4(textOutput('warn1')),
+  h4(textOutput('warn2')),
+  h4(textOutput('warn3')),
+  br(),br(),
+
+  #OUTPUT
+  radioButtons("ComparisonCriterion", em(strong("Comparison criterion")),
+	c(Performance = "1", Designs = "2"),selected="Performance"),	
+  br(),
+  conditionalPanel(condition = "input.ComparisonCriterion == '2'",
+    em(strong("Designs")),
+    #br(), br(),
+    tabsetPanel(
+	tabPanel("Adaptive",
+		tableOutput("adaptive_design_sample_sizes_and_boundaries_table"),
+    my_plotOutput("adapt_boundary_plot") ),
+	tabPanel("Fixed, Total Population",
+		br(), br(),
+		tableOutput("fixed_H0C_design_sample_sizes_and_boundaries_table"),
+    my_plotOutput("fixed_HOC_boundary_plot") ) ,
+	tabPanel("Fixed, Subpop. 1 only",
+		br(), br(), br(), br(), br(),
+		tableOutput("fixed_H0S_design_sample_sizes_and_boundaries_table"),
+    my_plotOutput("fixed_HOS_boundary_plot") ),
+	tabPanel("All designs",
+		tableOutput("adaptive_design_sample_sizes_and_boundaries_table.2"),
+		tableOutput("fixed_H0C_design_sample_sizes_and_boundaries_table.2"),
+		tableOutput("fixed_H0S_design_sample_sizes_and_boundaries_table.2"))
+    )),
+    #HTML("<hr>"),
+    #em(strong("Performance comparisons")),
+    #br(), br(),
+  conditionalPanel(condition = "input.ComparisonCriterion == '1'",
+    tabsetPanel(
+	tabPanel("Power", my_plotOutput("power_curve_plot")),
+	tabPanel("Sample Size", my_plotOutput("expected_sample_size_plot")),
+	tabPanel("Duration", my_plotOutput("expected_duration_plot")),
+	tabPanel("Overruns", my_plotOutput("overruns"))
+    ),
+    br(),
+    tableOutput("performance_table"))
+  #,br(),textOutput('uploadTime'),textOutput('loadReset')
+  )
+))
