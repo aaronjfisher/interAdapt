5fe5072084726a9e7e8fb60b1877fd66d110e1aa
Aaron Fisher <afisher@jhsph.edu>
2014-11-29 00:15:24 -0500

    add yes/no instructions

--- r_package/R/zzz.R
+++ r_package/R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
 	if(interactive()) {
-		ans <- readline("Launch the interAdapt app? ")
+		ans <- readline("Launch the interAdapt app? (type 'yes' or 'no')")
 		if(tolower(substr(ans, 1, 1)) == "y")
 			runInterAdapt()
 	}
